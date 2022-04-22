709c7991dec3f11ab16bfa7e5dbfabc82929218c
Jason <jimanproductions@gmail.com>
2022-04-22 12:41:53 +0200

    intersection observer functional

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const cards = document.querySelectorAll(".card")
 
 const observer = new IntersectionObserver(entries => {
-    console.log(entries)
+    entries.forEach(entry=>{
+        entry.target.classList.toggle("show", entry.isIntersecting)
+        //if entry is visible, then take it away
+        if (entry.isIntersecting) observer.unobserve(entry.target)
+    })
+}, {
+    threshold: 1
+    // console.log(entries)
 })
 
-observer.observe(cards[0])
+cards.forEach(card =>{
+    observer.observe(card)
+})
